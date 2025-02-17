3b53a9faf210f09656f277d0d92ffd190544440f
Priyanshu Biswal <priy.biswal@gmail.com>
2025-02-16 19:30:05 -0500

    fixes to firebase

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,8 +1,8 @@
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-// import firebase from "../node_modules/firebase/app";
-// import "../node_modules/firebase/firestore";
+import firebase from "../node_modules/firebase/app";
+import "../node_modules/firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
@@ -10,118 +10,144 @@
 
 
 // Initialize Cloud Firestore and get a reference to the service
-// const db = firebase.firestore();
+const db = firebase.firestore();
 
 
-// db.collection("scouters").add({
-//     name: "Priyanshu Biswal",
-//     permission: "admin"
-// })
-// .then((docRef) => {
-//     console.log("Document written with ID: ", docRef.id);
-// })
-// .catch((error) => {
-//     console.error("Error adding document: ", error);
-// });
+db.collection("scouters").add({
+    name: "Priyanshu Biswal",
+    permission: "admin"
+})
+.then((docRef) => {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch((error) => {
+    console.error("Error adding document: ", error);
+});
 
 
-// db.collection("users").get().then((querySnapshot) => {
-//     querySnapshot.forEach((doc) => {
-//         console.log(`${doc.id} => ${doc.data()}`);
-//     });
-// });
+db.collection("users").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+});
 
-// class CachedResponse {
-//     constructor() {
-//         this.collection = null;
-//         this.data = null;
-//     }
 
-//     open(collectionName) {
-//         this.collection = collectionName;
-//         return this;
-//     }
 
-//     add(dataObject) {
-//         this.data = dataObject;
-//         return this;
-//     }
 
-//     async submit() {
-//         if (this.collection !== null && this.data !== null) {
-//             return await db.collection(this.collection).add(this.data);
-//         } else {
-//             console.log("Unable to send a response to database: Missing either collection or data");
-//         }
-//     }
 
-//     addFilters(...filterList) { // Little bit different addFilters({attribute: "", condition})
 
-//     }
-// }
 
-// class CachedRequest {
-//     constructor() {
-//         this.collection = null;
-//         this.filters = [];
-//         this.attributes = []; 
-//     }
 
-//     open(collectionName) {
-//         this.collection = collectionName;
-//         return this;
-//     }
 
-//     returnAttributes(...attributeList) {
-//         this.attributes = this.attributes.concat(attributeList);
-//         return this;
-//     }
 
-//     addFilters(...filterList) {
-//         this.filters = this.filters.concat(filterList);
-//         return this;
-//     }
 
-//     async withdraw() {
-//         let data;
-//         if (this.collection !== null) {
-//             data = await new Promise((resolve, reject) => {
-//                 db.collection(this.collection).get().then(querySnapshot => {
-//                     let currentData = [];
-//                     querySnapshot.forEach(doc => currentData.push(doc.data()));
 
-//                     if (this.filters.length > 0) {
-//                         this.filters.forEach(filter => {
-//                             let filteredData = [];
-//                             currentData.forEach(doc => {
-//                                 if (filter.condition(doc[filter.attribute])) filteredData.push(doc);
-//                             });
-//                             currentData = filteredData;
-//                         });
-//                     }
 
-//                     if (this.attributes.length > 0) {
-//                         let returnableData = [];
-//                         currentData.forEach(doc => {
-//                             let revisedDoc = {};
-//                             this.attributes.forEach(attr => {
-//                                 revisedDoc[attr] = doc[attr];
-//                             });
-//                             returnableData.push(revisedDoc);
-//                         });
-//                         currentData = returnableData;
-//                     }
 
-//                     resolve(currentData);
-//                 });
-//             });
 
-//             return data;
-//         } else {
-//             console.log("Unable to request data from the database: Missing collection");
-//         }
-//     }
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+class CachedResponse {
+    constructor() {
+        this.collection = null;
+        this.data = null;
+    }
+
+    open(collectionName) {
+        this.collection = collectionName;
+        return this;
+    }
+
+    add(dataObject) {
+        this.data = dataObject;
+        return this;
+    }
+
+    async submit() {
+        if (this.collection !== null && this.data !== null) {
+            return await db.collection(this.collection).add(this.data);
+        } else {
+            console.log("Unable to send a response to database: Missing either collection or data");
+        }
+    }
+
+    addFilters(...filterList) { // Little bit different addFilters({attribute: "", condition})
+
+    }
+}
+
+class CachedRequest {
+    constructor() {
+        this.collection = null;
+        this.filters = [];
+        this.attributes = []; 
+    }
+
+    open(collectionName) {
+        this.collection = collectionName;
+        return this;
+    }
+
+    returnAttributes(...attributeList) {
+        this.attributes = this.attributes.concat(attributeList);
+        return this;
+    }
+
+    addFilters(...filterList) {
+        this.filters = this.filters.concat(filterList);
+        return this;
+    }
+
+    async withdraw() {
+        let data;
+        if (this.collection !== null) {
+            data = await new Promise((resolve, reject) => {
+                db.collection(this.collection).get().then(querySnapshot => {
+                    let currentData = [];
+                    querySnapshot.forEach(doc => currentData.push(doc.data()));
+
+                    if (this.filters.length > 0) {
+                        this.filters.forEach(filter => {
+                            let filteredData = [];
+                            currentData.forEach(doc => {
+                                if (filter.condition(doc[filter.attribute])) filteredData.push(doc);
+                            });
+                            currentData = filteredData;
+                        });
+                    }
+
+                    if (this.attributes.length > 0) {
+                        let returnableData = [];
+                        currentData.forEach(doc => {
+                            let revisedDoc = {};
+                            this.attributes.forEach(attr => {
+                                revisedDoc[attr] = doc[attr];
+                            });
+                            returnableData.push(revisedDoc);
+                        });
+                        currentData = returnableData;
+                    }
+
+                    resolve(currentData);
+                });
+            });
+
+            return data;
+        } else {
+            console.log("Unable to request data from the database: Missing collection");
+        }
+    }
+}
 
 /*
 
